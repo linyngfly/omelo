06321f5c3d88aa0e5a340f5983294f5c948daaad
linyngfly <linyngfly@163.com>
2018-02-09 01:57:14 +0800

    修改项目模板

---
+++ bin/test/app.js
@@ -0,0 +1,28 @@
+let pomelo = require('pomelo');
+
+/**
+ * Init app for client.
+ */
+let app = pomelo.createApp();
+app.set('name', 'test');
+
+// app configuration
+app.configure('production|development', 'connector', function(){
+  app.set('connectorConfig',
+    {
+      connector : pomelo.connectors.sioconnector,
+      // 'websocket', 'polling-xhr', 'polling-jsonp', 'polling'
+      transports : ['websocket', 'polling'],
+      heartbeats : true,
+      closeTimeout : 60 * 1000,
+      heartbeatTimeout : 60 * 1000,
+      heartbeatInterval : 25 * 1000
+    });
+});
+
+// start app
+app.start();
+
+process.on('uncaughtException', function (err) {
+  console.error(' Caught exception: ' + err.stack);
+});
