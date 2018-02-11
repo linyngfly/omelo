a8bf5a3a604f6c55f9f2a7dea74f409e2adf24ef
linyngfly <linyngfly@163.com>
2018-02-11 10:34:16 +0800

    模板修改

--- template/app.js
+++ template/app.js
@@ -1,4 +1,6 @@
-let omelo = require('omelo');
+const omelo = require('omelo');
+const path = require('path');
+const omeloHttpPlugin = require('omelo-http-plugin');
 
 /**
  * Init app for client.
@@ -7,14 +9,25 @@ let app = omelo.createApp();
 app.set('name', '$');
 
 // app configuration
-app.configure('production|development', 'connector', function(){
-  app.set('connectorConfig',
-    {
-      connector : omelo.connectors.hybridconnector,
-      heartbeat : 3,
-      useDict : true,
-      useProtobuf : true
-    });
+app.configure('production|development', 'connector', function () {
+  app.set('connectorConfig', {
+    connector: omelo.connectors.hybridconnector,
+    heartbeat: 3,
+    useDict: true,
+    useProtobuf: true
+  });
+});
+
+app.configure('production|development', 'http', function () {
+  app.loadConfig('http', path.join(app.getBase(), 'config/http.json'));
+  app.use(omeloHttpPlugin, {
+    http: app.get('http')
+  });
+
+  app.beforeStopHook(function () {
+    //todo add user code
+  });
+
 });
 
 // start app
